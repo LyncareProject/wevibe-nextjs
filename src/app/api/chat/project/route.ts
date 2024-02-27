724789b6467112e16da6b1912f60bd96769c205d
Kun Woo Kim <kimkuns98@gmail.com>
2024-02-27 16:51:38 +0900

    AI

--- src/app/api/chat/project/route.ts
+++ src/app/api/chat/project/route.ts
@@ -1,5 +1,6 @@
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -10,12 +11,20 @@ export const runtime = 'edge';
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+  let conversationHistory: ChatCompletionMessageParam[] = [
+    {
+      role: 'system',
+      content:
+        "You are an outstanding web app designer. Answer customer questions related to web apps in an easy-to-understand manner. If a customer asks something unrelated to web apps, respond as if you don't have information on that topic. Please respond in Korean.",
+    },
+  ];
+  conversationHistory = conversationHistory.concat(messages); 
 
-  // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4-1106-preview',
     stream: true,
-    messages,
+    messages: conversationHistory,
+    temperature: 0.7,
   });
 
   // Convert the response into a friendly text-stream
